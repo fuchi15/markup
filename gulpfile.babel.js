765dcf47accfafbca48dab2412829bf451170844
fuchi15 <sakuya815@gmail.com>
2017-12-08 15:02:22 +0900

    rpg-top 作成

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -19,7 +19,7 @@ import webpackConfig from './webpack.config.js'
  */
 
 const paths = {
-    rootDir: 'C:/Users/anli/Documents/GitHub/oic-rpg-markup/markup-master',
+    rootDir: '/Applications/MAMP/htdocs/oic/markup/',
     serverDir: 'localhost'
 };
 
